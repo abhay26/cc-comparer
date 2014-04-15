559cff3b784d3e5a60faf1e44a9131b869524819
Abhay <abhaymangal26@gmail.com>
2014-04-15 22:29:40 +0530

    adding initial files

--- main.py
+++ main.py
@@ -92,4 +92,6 @@ def display():
 
 if __name__ == '__main__':
 	# init_db()
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+	#app.run(debug=True)
